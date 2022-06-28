a5c694cdd888722b877a6f91881a2ea0865df8ce
MatteoGenovese <matteo.genovese@icloud.com>
2022-06-28 11:31:49 +0200

    Submit

---
+++ Peso tot peperoni/script.js
@@ -0,0 +1,61 @@
+peperoni = [
+
+    {
+        nome: "Peperone di Pontecorvo Dop",
+        peso: 1,
+        lunghezza: 20
+    },
+    {
+        nome: "Peperone di Carmagnola Igp",
+        peso: 1,
+        lunghezza: 23
+    },
+    {
+        nome: "Peperone di Senise Igp",
+        peso: 1,
+        lunghezza: 23
+    },
+    {
+        nome: "Peperone Quadrato di Asti",
+        peso: 1,
+        lunghezza: 23
+    },
+    {
+        nome: "Peperone di Capriglio",
+        peso: 1,
+        lunghezza: 23
+    },
+    {
+        nome: "Papaccella napoletana",
+        peso: 1,
+        lunghezza: 23
+    },
+    {
+        nome: "Friggitello Torricello",
+        peso: 1,
+        lunghezza: 23
+    },
+    {
+        nome: "Peperone dolce di Altino",
+        peso: 1,
+        lunghezza: 23
+    },
+    {
+        nome: "Peperone di Polizzi Generosa",
+        peso: 1,
+        lunghezza: 23
+    },
+    {
+        nome: "Cappello del Vescovo",
+        peso: 1,
+        lunghezza: 23
+    }
+]
+
+let pesoTot = 0
+for (let peperone in peperoni) {
+    pesoTot = pesoTot + peperoni[peperone].peso;
+    console.log(peperoni[peperone].peso)
+}
+
+console.log(pesoTot);
